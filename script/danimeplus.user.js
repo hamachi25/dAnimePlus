dc7bdfefe7d474df2dce9c4829e17623c189c2ae
chimaha <107383950+chimaha@users.noreply.github.com>
2023-08-05 21:42:22 +0900

    バージョン

--- script/danimeplus.user.js
+++ script/danimeplus.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/chimaha/dAnimePlus
 // @match       https://animestore.docomo.ne.jp/animestore/*
 // @grant       none
-// @version     1.6
+// @version     1.6.1
 // @author      chimaha
 // @description dアニメストアに様々な機能を追加します
 // @license     MIT license
